6c92f49205ffd6d57dccc7d33cfb84619dcc6c4c
foliwe <foliwe@gmail.com>
2020-02-01 17:16:18 +0000

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   get '/home/help'
   get '/home/contact'
   get '/home/index'
-
   get '/signup',  to: 'users#new'
-
+  resources :users
   get '/check.txt', to: proc {[200, {}, ['it_works']]}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
