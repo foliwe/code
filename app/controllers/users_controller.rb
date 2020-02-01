60adba5344c783befa25f7668c49879a5491efd2
foliwe <foliwe@gmail.com>
2020-02-01 22:35:39 +0000

    autenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -6,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -18,9 +21,37 @@ class UsersController < ApplicationController
     
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+    render 'edit'
+    end
+  end
+  
+
   private
     def user_params
      params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless  current_user?(@user)
     end
 
   
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,10 +4,16 @@ module SessionsHelper
     end
 
     def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+          @current_user ||= User.find_by(id: user_id)
+        elsif (user_id = cookies.encrypted[:user_id])
+          user = User.find_by(id: user_id)
+          if user && user.authenticated?(:remember, cookies[:remember_token])
+            log_in user
+            @current_user = user
+          end
         end
-    end
+      end
 
     def logged_in?
         !current_user.nil?
@@ -16,5 +22,19 @@ module SessionsHelper
     def log_out
         session.delete(:user_id)
         @current_user = nil
+    end
+
+    def current_user?(user)
+        user && user == current_user
+    end
+
+    # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+    end
+    # Stores the URL trying to be accessed.
+    def store_location
+    session[:forwarding_url] = request.original_url if request.get?
     end
 end
