7ea863be4db8d1aa7048d97cf29dc304d2ec1fee
foliwe <foliwe@gmail.com>
2020-02-01 05:39:21 +0000

    Password to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root to: 'home#index'
-  get 'home/about'
-  get 'home/help'
-  get 'home/contact'
-  get 'home/index'
+  get '/home/about'
+  get '/home/help'
+  get '/home/contact'
+  get '/home/index'
+
+  get '/signup',  to: 'users#new'
+
   get '/check.txt', to: proc {[200, {}, ['it_works']]}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
